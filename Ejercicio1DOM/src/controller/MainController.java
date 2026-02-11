166c6401b5f903cf754c52c5cf100f1f63d85625
Toothless <Toothless@DESKTOP-CMDRIJG>
2026-02-11 19:10:50 +0100

    Version de Java cambiadad a la 1.8

---
+++ Ejercicio1DOM/src/controller/MainController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import model.Data;
+import view.MainWindow;
+
+public class MainController {
+	
+	private MainWindow window = new MainWindow();
+	private Data data = new Data();
+
+	public MainController() {
+		
+	}
+}
